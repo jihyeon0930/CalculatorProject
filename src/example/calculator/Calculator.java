f9f3275cc3c718c1f6eb7d70fdf0f3c981613336
jihyeon <bjh20049@naver.com>
2026-03-25 12:46:01 +0900

    STEP 2 : 완성

--- src/example/calculator/Calculator.java
+++ src/example/calculator/Calculator.java
@@ -4,48 +4,9 @@ import java.util.ArrayList;
 
 public class Calculator {
 
-    //연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성
-    private ArrayList<Integer> results = new ArrayList<>();
 
-    //양의 정수 2개와 연산기호 매개변수로 받기
-    public int calculate(char op, int num1, int num2) {
-        int result = 0;
-        switch (op) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없음");
-                } else {
-                    result = num1 / num2;
-                }
-                break;
-            default:
-                System.out.println("다시 입력해 주세요");
-        }
 
-        //연산 결과 추가
-        results.add(result);
-        return result;
-    }
 
-    public ArrayList<Integer> getResults() {
-        return results;
-    }
 
-    public void setResults(ArrayList<Integer> results) {
-        this.results = results;
-    }
 
-    //가장 먼저 저장된 데이터 삭제 기능
-    public void resultRemove() {
-        results.remove(0);
-    }
 }
--- src/example/calculator/Main.java
+++ src/example/calculator/Main.java
@@ -8,10 +8,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator(); //인스턴스화
+        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator();
+
 
         System.out.println(":::계산기 프로그램 시작");
 
-        String q;
+        String q = "";
 
         do {
             System.out.print("첫번 째 수 입력 : ");
@@ -19,16 +21,22 @@ public class Main {
 
             System.out.print("연산 기호 입력(+, -, *, /) : ");
             String opInput = sc.next();
-            char op = opInput.charAt(0);
+
+            OperatorType op = OperatorType.formSymbol(opInput); //formsymbol()메서드 호출, opInput 전달(OperatorType에선 symbol)
+
+            if (op == null) {
+                System.out.println("잘못된 연산자 입력, 재입력");
+                continue;
+            }
 
             System.out.print("두번 째 수 입력 : ");
             int num2 = sc.nextInt();
 
-            //Calculator 클래스
-            int result = calculator.calculate(op, num1, num2);
-            //result = calculatorFunction(op, num1, num2);
+            //arithmeticCalculator 기능 가져오기
+            int result = arithmeticCalculator.calculate(num1, num2, op);
 
-            System.out.println(num1 +" "+ op + " " + num2 +" = "+result);
+
+            System.out.println(num1 +" "+ op.getSymbol() + " " + num2 +" = "+result);
             System.out.print("계속 하려면 아무 키, 종료하려면 exit 입력(삭제시 remove 입력) : ");
             q = sc.next();
 
@@ -36,11 +44,11 @@ public class Main {
 
         //remove기능
         if (q.equals("remove")) {
-            calculator.resultRemove();
+            arithmeticCalculator.resultRemove();
         }
 
         //getter
-        ArrayList<Integer> results = calculator.getResults();
+        ArrayList<Integer> results = arithmeticCalculator.getResults();
         System.out.println("계산 목록 : " + results);
         
 
